33bd3501056764914b36819f1d4ef160fba2c564
Diego Echeverri <diego@geeny.io>
2017-11-29 15:54:51 +0100

    Simplifying upload command

--- to_rpi.sh
+++ to_rpi.sh
@@ -4,15 +4,16 @@ RPI_HOST=$1
 RPI_PATH=$2
 
 BINARY_PATH=hub-sdk/target/armv7-unknown-linux-gnueabihf/release/hub-service
+CONFIG_PATH=hub-sdk/geeny_hub_service.mvdb.json
 CREDENTIALS_PATH=hub-sdk/credentials.mvdb.json
 ELEMENTS_PATH=hub-sdk/elements.mvdb.json
 CERTIFICATES_PATH=hub-sdk/certificates
 
-if [ -z $RPI_PATH]; then
+if [ -z $RPI_PATH ]; then
     RPI_PATH="~/"
 fi
 
-if [ -z $RPI_HOST]; then
+if [ -z $RPI_HOST ]; then
     echo "USAGE:"
     echo "    ./to_rpi <HOST>"
     echo ""
@@ -21,10 +22,12 @@ if [ -z $RPI_HOST]; then
 else
     HOST_PATH="$RPI_HOST:$RPI_PATH"
     echo "Uploading to: $HOST_PATH"
-    scp    "$CREDENTIALS_PATH"  "$HOST_PATH"
-    echo "* Uploaded: $ELEMENTS_PATH"
-    scp    "$ELEMENTS_PATH"     "$HOST_PATH"
-    echo "* Uploaded: $CREDENTIALS_PATH"
-    scp -r "$CERTIFICATES_PATH" "$HOST_PATH"
-    echo "* Uploaded: $CERTIFICATES_PATH"
+
+    scp -r \
+        "$BINARY_PATH"\
+	"$CONFIG_PATH"\
+	"$CREDENTIALS_PATH"\
+	"$ELEMENTS_PATH"\
+	"$CERTIFICATES_PATH"\
+	"$HOST_PATH"
 fi
